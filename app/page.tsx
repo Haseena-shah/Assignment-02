c32d3d71121956ba06986f2ca79aec76fff81ebe
Pir-Zohaib-Shah <Zohaibshahak47@gmail.com>
2024-09-25 18:42:54 +0500

    class 2

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from './components/About'
+import Header from './components/Header'
+
+const Home = () => {
+  return (
+  <>
+  <div>Home page</div>
+  <About/> <Header/>
+  </>  
+  )
+}
+
+export default Home
